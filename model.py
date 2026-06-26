f1a1ac190d42b2dcbfb7c8865d5b03e23299ac95
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2026-06-26 11:28:53 -0700

    add mode enum, add text buffer as abstract base class, add tuple buffer as concrete naiv implementation

--- model.py
+++ model.py
@@ -1,3 +1,33 @@
+from enum import Enum
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+   
+class TextBuffer(ABC):
+    
+    @abstractmethod
+    def delete_character(self, position: int) -> "TextBuffer":
+        pass
+    
+    @abstractmethod
+    def insert_character(self, position: int, character: str) -> "TextBuffer":
+        pass
+
+@dataclass(frozen=True) 
+class TupleBuffer(TextBuffer):
+    
+    buffer : tuple[str, ...] = ()
+    
+    def delete_character(self, position: int) -> "TupleBuffer":
+        return TupleBuffer(self.buffer[:position] + self.buffer[position + 1:])
+    
+    def insert_character(self, position: int, character: str) -> "TupleBuffer":
+        return TupleBuffer(self.buffer[:position] + (character,) + self.buffer[position:])
+        
+class Mode(Enum):
+    NORMAL = 1
+    INSERT = 2
+    
 @dataclass(frozen=True)
 class EditorModel:
     
