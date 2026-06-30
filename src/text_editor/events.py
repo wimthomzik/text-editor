218a440f48aaadda9908ff9bd1c288b8f7c12dee
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2026-06-30 15:28:37 -0700

    remove higher level event quit to handle policy in update; add more special character events

--- src/text_editor/events.py
+++ src/text_editor/events.py
@@ -1,8 +1,22 @@
 from dataclasses import dataclass
+from enum import Enum
+
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
 
 @dataclass(frozen=True)
 class Char:
     char: str
+    
+@dataclass(frozen=True)
+class Escape: ...
+
+@dataclass(frozen=True)
+class Arrow():
+    direction: Direction
 
 @dataclass(frozen=True)
 class Backspace: ...
@@ -10,7 +24,4 @@ class Backspace: ...
 @dataclass(frozen=True)
 class Enter: ...
 
-@dataclass(frozen=True)
-class Quit: ...
-
-Event = Char | Backspace | Enter | Quit
+Event = Char | Backspace | Enter | Arrow | Escape
